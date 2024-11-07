a54c7743bcfd7965df13b0c9f5c7ac56b26d80dd
YifanZhou99 <75839011+YifanZhou99@users.noreply.github.com>
2024-11-07 15:51:48 +0100

    add image analyse function with genemi

--- experiments/llm/hello_world.py
+++ experiments/llm/hello_world.py
@@ -4,11 +4,18 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv() 
-
 gemini_api_key = os.getenv('GEMINI_API_KEY')
-print(gemini_api_key)
-#genai.configure(api_key=os.environ["API_KEY"])
 genai.configure(api_key=gemini_api_key)
+
+
 model = genai.GenerativeModel("gemini-1.5-flash")
+
+
 response = model.generate_content("Hello!")
 print(response.text)
+
+myfile = genai.upload_file("./images/tram.jpg")
+result = model.generate_content(
+    [myfile, "\n\n", "Describe this photo to a blind person to help them understand where they are and for navigation purposes."],
+)
+print(result.text)
