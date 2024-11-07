96c69cd7fee3b37cf3b44aa2443cc503592c5326
YifanZhou99 <75839011+YifanZhou99@users.noreply.github.com>
2024-11-07 15:12:57 +0100

    call gemini api

---
+++ experiments/llm/test.py
@@ -0,0 +1,11 @@
+
+import google.generativeai as genai
+import os
+
+
+
+#genai.configure(api_key=os.environ["API_KEY"])
+genai.configure(api_key=gemini_gpi)
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Write a story about a magic backpack.")
+print(response.text)
